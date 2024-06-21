79714164a2b6858d10f708819740fdd53bab3e65
William Keay <149808948+w-keay@users.noreply.github.com>
2024-06-21 13:01:34 +0100

    Added Multiple Choice Coding Questions

--- src/main/java/tech/makers/demo/levels/LevelManager.java
+++ src/main/java/tech/makers/demo/levels/LevelManager.java
@@ -5,6 +5,8 @@ import tech.makers.demo.EscapeRoomGame;
 import tech.makers.demo.assets.Door;
 import tech.makers.demo.player.Player;
 
+import java.util.Arrays;
+
 public class LevelManager {
     private Level[] levels;
     private int currentLevelIndex;
@@ -21,12 +23,42 @@ public class LevelManager {
     protected void initializeLevels() {
         // Create players, puzzles, and doors for each level
         Player player1 = new Player(100, 100);
-        Puzzle puzzle1 = new Puzzle(300, 300, "What is 2 + 2?", "4", "/sprites/Computer.png");
+        Puzzle puzzle1 = new Puzzle(
+                300,
+                300,
+                "What will this code output?\n\n" +
+                        "public class Main {\n" +
+                        "    public static void main(String[] args) {\n" +
+                        "        int x = 10;\n" +
+                        "        x += 5;\n" +
+                        "        System.out.println(x);\n" +
+                        "    }\n" +
+                        "}\n",
+                "15",
+                Arrays.asList("5", "10", "15", "20"),
+                "/sprites/Computer.png"
+        );
         Door door1 = new Door(600, 400, "/sprites/door.png");
         Level level1 = new Level(player1, puzzle1, door1);
 
         Player player2 = new Player(100, 100);
-        Puzzle puzzle2 = new Puzzle(300, 300, "What is the capital of France?", "Paris", "/sprites/Computer 2.png");
+        Puzzle puzzle2 = new Puzzle(
+                300,
+                300,
+                "What will this code output?\n\n" +
+                        "import org.junit.Test;\n" +
+                        "import static org.junit.Assert.*;\n\n" +
+                        "public class TestExample {\n" +
+                        "    @Test\n" +
+                        "    public void addition() {\n" +
+                        "        int sum = 2 + 3;\n" +
+                        "        assertEquals(5, sum);\n" +
+                        "    }\n" +
+                        "}\n",
+                "Test passed",
+                Arrays.asList("Compilation error", "Runtime error", "Test failed", "Test passed"),
+                "/sprites/Computer 2.png"
+        );
         Door door2 = new Door(600, 400, "/sprites/Door 2.png");
         Level level2 = new Level(player2, puzzle2, door2);
 
